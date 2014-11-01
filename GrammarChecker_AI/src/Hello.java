a605e6199c7c1013d46068c45b66a899510bcc5c
Isacar Racine <irodrigu@harding.edu>
2014-10-31 21:51:43 -0500

    Hello World :)

---
+++ GrammarChecker_AI/src/Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello " + name);
+	}
+
+}
